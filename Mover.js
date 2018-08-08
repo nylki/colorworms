eb1802d8fd61207abd9b65f43a7eb4cf1d72208f
Tom <tombrwe@gmail.com>
2018-08-08 10:47:32 +0200

    performance improvements

--- Mover.js
+++ Mover.js
@@ -1,39 +1,38 @@
-class Mover{
-
+class Mover {
+ 
   constructor(c) {
+    
     this.location = createVector(width/2, height/2);
     this.plocation = createVector(width/2, height/2);
-    this.velocity = createVector(0,0);
+    this.velocity = createVector(0, 0);
     this.acceleration = createVector(0, -0);
     this.dimension = createVector(2, 2);
-    this.n = Math.random();
+    this.randomSeed = Math.random();
     this.c = c;
   }
 
 
-  update(){
+  update(globalTime) {
 
     this.location.x = constrain(this.location.x, 0, width);
     this.location.y = constrain(this.location.y, 0, height);
 
-    this.n = this.n + 0.001;
-    let perlinNoise = map(noise(this.n), 0, 1, -0.5, 2);
+    let perlinNoise = map(noise(this.randomSeed + globalTime), 0, 1, -0.5, 2);
 
     this.acceleration = p5.Vector.random2D();
     this.acceleration.mult(perlinNoise);
 
     this.velocity.limit(1);
     this.velocity.add(this.acceleration);
-    this.velocity.mult(map(mouseY, 0, height, 0,5));
+    this.velocity.mult(map(mouseY, 0, height, 0, 5));
 
     this.plocation.set(this.location.x, this.location.y);
     this.location.add(this.velocity);
 
   }
 
-  display(){
-    stroke(this.c,200);
-    strokeWeight( map(mouseX, 0, width, 1, 20));
+  display() {
+    stroke(this.c, 200);
     line(this.plocation.x, this.plocation.y, this.location.x, this.location.y);
   }
 
--- index.js
+++ index.js
@@ -1,15 +1,16 @@
-const MAX_MOVERS = 650;
+const MAX_MOVERS = 600;
+var globalTime = 0;
+var movers = [];
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
   colorMode(HSB, 255);
   fill(0, 5);
 
-  movers = [];
-  let randomHue = random(0,100);
+  let randomHue = random(0, 100);
   for (let i=0; i<=MAX_MOVERS; i++) {
     let n = map(noise(i), 0, 1, -110, 110);
-    let c = color(randomHue + n, random(140, 180) + n, random(100,200) + n);
+    let c = color(randomHue + n, random(140, 180) + n, random(100, 200) + n);
     movers.push(new Mover(c)); //color is perlinnoise randomnes
   }
 
@@ -18,9 +19,10 @@ function setup() {
 }
 
 function draw() {
-
-  for (let mover  of movers) {
-    mover.update();
+  globalTime += 0.001;
+  strokeWeight(map(mouseX, 0, width, 1, 20));
+  for (let mover of movers) {
+    mover.update(globalTime);
     mover.display();
   }
 
